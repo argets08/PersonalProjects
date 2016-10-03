cb05fa1b145cb0309052fe23143bf6bf8712cec1
Tanmaya Kumar <tanmayakg@gmail.com>
2016-10-03 16:57:43 -0500

    Update to destroy directory structure and rename all files, added new panel for sort processing

--- PhotoOrg/src/fileMover.java
+++ PhotoOrg/src/fileMover.java
@@ -5,10 +5,12 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.lang.Object;
+import org.apache.commons.io.*;
 
 public class fileMover {
 
-	public fileMover() {}
+	public fileMover() {
+	}
 
 	// FileUtils.copyDirectory(srcDir, destDir);
 
@@ -20,7 +22,7 @@ public class fileMover {
 		}
 	}
 
-	private void copyDirectory(File source, File target) throws IOException {
+	void copyDirectory(File source, File target) throws IOException {
 		if (!target.exists()) {
 			target.mkdir();
 		}
@@ -30,18 +32,72 @@ public class fileMover {
 		}
 	}
 
-	private void copyFile(File source, File target) throws IOException {
-		try (
-				InputStream in = new FileInputStream(source);
-				OutputStream out = new FileOutputStream(target)
-		) {
+	void copyFile(File source, File target) throws IOException {
+		try (InputStream in = new FileInputStream(source); OutputStream out = new FileOutputStream(target)) {
 			byte[] buf = new byte[1024];
 			int length;
 			while ((length = in.read(buf)) > 0) {
-				 out.write(buf, 0, length);
+				out.write(buf, 0, length);
 			}
 			in.close();
- 	        out.close();
+			out.close();
 		}
 	}
+
+	void destroyArchitecture(File source) throws IOException {
+		// sequence iterator
+		int value = 1;
+		File[] listOfFiles;
+		File[] directories = new File(source.getPath()).listFiles(File::isDirectory);
+		for (File d : directories) {
+			listOfFiles = d.listFiles();
+			if (listOfFiles != null) {
+				for (File child : listOfFiles) {
+
+					String[] fileNameSplits = child.getName().split("\\.");
+					// extension is assumed to be the last part
+					int extensionIndex = fileNameSplits.length - 1;
+					// add extension to id
+					File newName = new File(
+							source.getPath() + "\\PHOTORG_" + value + "." + fileNameSplits[extensionIndex]);
+
+					// Move files to destination folder
+					child.renameTo(newName);
+					value++;
+					System.out.println(newName.getName());
+				}
+
+				// Add if you want to delete the source folder
+				d.delete();
+			}
+		}
+		listOfFiles = source.listFiles();
+		if (listOfFiles != null) {
+			for (File child : listOfFiles) {
+
+				// maintain file format (jpeg, jpg, png...)
+				String[] fileNameSplits = child.getName().split("\\.");
+				// extension is assumed to be the last part
+				int extensionIndex = fileNameSplits.length - 1;
+				// add extension to id
+				File newName = new File(source.getPath() + "\\PHOTORG_" + value + "." + fileNameSplits[extensionIndex]);
+
+				// Move files to destination folder
+				child.renameTo(newName);
+				value++;
+				// Delete original
+				child.delete();
+				System.out.println(newName.getName());
+			}
+		} else {
+			System.out.println(source + "  Folder does not exists");
+		}
+		System.out.println("File creation @ Target complete");
+	}
+//may need to flesh this out
+	void renameAll(File source, String title) throws IOException {
+					File[] listOfFiles = source.listFiles();
+					
+					
+		}
 }
