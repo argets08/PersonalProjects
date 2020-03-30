dab01b510e01aad27c1c6fb13674e9017c415b5a
Tanmaya Kumar <tanmayakg@gmail.com>
2020-03-29 22:59:57 -0400

    base start

---
+++ src/test/java/problems/LinkedListsTest.java
@@ -0,0 +1,14 @@
+package problems;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LinkedListsTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+}
